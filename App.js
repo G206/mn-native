3e90f3698d01811442637ec76be2a5fdc08acc7b
Gabe <w3dev@yahoo.com>
2018-08-08 00:03:38 -0700

    Added new 3rd part libray react-native-track-player. Getting errors.

--- App.js
+++ App.js
@@ -3,17 +3,107 @@ import {Platform, StyleSheet, Text, View} from 'react-native';
 import Nav from './components/nav';
 import Display from './components/display';
 import Controls from './components/controls';
-import SoundPlayer from 'react-native-sound-player';
+//import SoundPlayer from 'react-native-sound-player';
+import TrackPlayer from 'react-native-track-player';
 
 export default class App extends React.Component {
     constructor(props) {
         super(props);
 
         this.state = {
-            keys: ['03_C','03_CsDf','03_D','03_DsEf','03_E','03_F','03_FsGf','03_G','03_GsAf',
-                '03_A','03_AsBf','03_B'],
+            keys: ['3_1','3_2','3_3','3_4','3_5','3_6','3_7','3_8','3_9',
+                '03_10','3_11','3_12'],
             message: ''
         };
+
+        this.playlist = [
+            {
+                id: '3_1',
+                url: require('./assets/audio/03_c.mp3'),
+                title: '3rd C'
+            },
+            {
+                id: '3_2',
+                url: require('./assets/audio/03_c#.mp3'),
+                title: '3rd C#'
+            },
+            {
+                id: '3_3',
+                url: require('./assets/audio/03_d.mp3'),
+                title: '3rd D'
+            },
+            {
+                id: '3_4',
+                url: require('./assets/audio/03_d#.mp3'),
+                title: '3rd D#'
+            },
+            {
+                id: '3_5',
+                url: require('./assets/audio/03_e.mp3'),
+                title: '3rd E'
+            },
+            {
+                id: '3_6',
+                url: require('./assets/audio/03_f.mp3'),
+                title: '3rd F'
+            },
+            {
+                id: '3_7',
+                url: require('./assets/audio/03_f#.mp3'),
+                title: '3rd F#'
+            },
+            {
+                id: '3_8',
+                url: require('./assets/audio/03_g.mp3'),
+                title: '3rd G'
+            },
+            {
+                id: '3_9',
+                url: require('./assets/audio/03_g#.mp3'),
+                title: '3rd G#'
+            },
+            {
+                id: '3_10',
+                url: require('./assets/audio/03_a.mp3'),
+                title: '3rd A'
+            },
+            {
+                id: '3_11',
+                url: require('./assets/audio/03_a#.mp3'),
+                title: '3rd A#'
+            },
+            {
+                id: '3_12',
+                url: require('./assets/audio/03_b.mp3'),
+                title: '3rd B'
+            },
+
+        ]
+    }
+
+    // subscribe to the finished playing event in componentDidMount
+//    componentDidMount() {
+//        SoundPlayer.onFinishedPlaying((success: boolean) => { // success is true when the sound is played
+//            console.log('Finished playing sound file', success)
+//        })
+//    }
+
+    // unsubscribe when unmount
+//    componentWillUnmount() {
+//        SoundPlayer.unmount()
+//    }
+
+    componentDidMount() {
+        TrackPlayer.setupPlayer();
+        TrackPlayer.updateOptions({
+            stopWithApp: true,
+            capabilities: [
+                TrackPlayer.CAPABILITY_PLAY,
+                TrackPlayer.CAPABILITY_STOP,
+                TrackPlayer.CAPABILITY_PLAY_FROM_ID,
+                TrackPlayer.CAPABILITY_PLAY_FROM_SEARCH,
+            ]
+        });
     }
 
     updateMessage = pText =>{
@@ -23,18 +113,27 @@ export default class App extends React.Component {
         })
     }
 
-    playSound = (pNote, pType) =>{
+    playSound = async (pNote, pType) =>{
         try {
-            SoundPlayer.playSoundFile(pNote, pType);
+//            SoundPlayer.playSoundFile(pNote, pType);
+//            SoundPlayer.playUrl('https://example.com/music.mp3');
+            TrackPlayer.reset();
+            let positionNote = this.state.keys.indexOf(pNote);
+            await TrackPlayer.add(this.playlist[positionNote]);
+            TrackPlayer.play();
+            console.log('Played: ' + pNote);
         } catch (e) {
-            console.log('Cannot play the sound file');
+            console.log('Cannot play the sound file: ' + pNote);
         }
     }
 
     keyAction = (pText, pNote) =>{
         this.updateMessage(pText);
+
         this.playSound(pNote, 'mp3');
     }
+
+
     render() {
         return (
             <View style={styles.container}>
